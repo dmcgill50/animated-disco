174526314d04eeb738bbb9e3c90dbc4bbb4dba00
David McGill <dmcgill50@gmail.com>
2024-12-27 23:12:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+  return {
+    skills: ['JavaScript', 'React', 'Node', 'Express', 'MongoDB'],
+    bio: 'I am a full-stack developer with 5 years of experience',
+  };
+}
